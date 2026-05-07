59fe155130aa76d641c694a2e0b57c889e1f302c
gordey9992 <146545641+gordey9992@users.noreply.github.com>
2026-05-07 14:37:20 +0300

    Create ScriptCommand.java

---
+++ src/main/java/me/gordey9992/scriptcommands/ScriptCommand.java
@@ -0,0 +1,39 @@
+package me.gordey9992.scriptcommands;
+
+import org.bukkit.command.*;
+
+public class ScriptCommand implements CommandExecutor {
+    
+    private final ScriptCommands plugin;
+    
+    public ScriptCommand(ScriptCommands plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        
+        if (args.length == 0) {
+            sender.sendMessage("§cИспользование: /script update");
+            return true;
+        }
+        
+        if (args[0].equalsIgnoreCase("update")) {
+            if (!sender.hasPermission("scriptcommands.update.accept")) {
+                sender.sendMessage("§cУ вас нет прав на обновление!");
+                return true;
+            }
+            
+            if (plugin.getPendingUpdateVersion() == null) {
+                sender.sendMessage("§cНет ожидающих обновлений! Проверьте доступность новой версии.");
+                return true;
+            }
+            
+            plugin.performUpdate(sender);
+            return true;
+        }
+        
+        sender.sendMessage("§cНеизвестная подкоманда. Используйте /script update");
+        return true;
+    }
+}
