d6a9f3e9b47c1c87610757908d45986ae8906fc0
gordey9992 <146545641+gordey9992@users.noreply.github.com>
2026-05-07 14:41:39 +0300

    Update UpdateCommand.java

--- src/main/java/me/gordey9992/scriptcommands/UpdateCommand.java
+++ src/main/java/me/gordey9992/scriptcommands/UpdateCommand.java
@@ -2,7 +2,7 @@ package me.gordey9992.scriptcommands;
 
 import org.bukkit.command.*;
 import java.io.*;
-import java.net.URL;
+import java.net.*;
 import java.nio.file.*;
 
 public class UpdateCommand implements CommandExecutor {
@@ -37,29 +37,24 @@ public class UpdateCommand implements CommandExecutor {
             String latestVersion = plugin.getLatestVersion();
             String downloadUrl = "https://github.com/gordey9992/ScriptCommands/releases/download/" + latestVersion + "/ScriptCommands-" + latestVersion + ".jar";
             
-            // Папка плагинов
             File pluginsDir = plugin.getDataFolder().getParentFile();
             File currentJar = new File(pluginsDir, "ScriptCommands-" + plugin.getDescription().getVersion() + ".jar");
-            File newJar = new File(pluginsDir, "ScriptCommands-" + latestVersion + ".jar");
             File tempJar = new File(pluginsDir, "ScriptCommands-update-temp.jar");
             
             sender.sendMessage("§eСкачивание " + downloadUrl);
             
-            // Скачиваем новый JAR
             try (InputStream in = new URL(downloadUrl).openStream()) {
                 Files.copy(in, tempJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
             }
             
             sender.sendMessage("§aФайл скачан, устанавливаю...");
             
-            // Удаляем старый, переименовываем новый
             if (currentJar.exists()) {
                 currentJar.delete();
             }
-            Files.move(tempJar.toPath(), newJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            Files.move(tempJar.toPath(), currentJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
             
             sender.sendMessage("§aОбновление установлено! Перезагрузите сервер или выполните §6/restart§a.");
-            sender.sendMessage("§7(При перезагрузке плагин загрузит новую версию)");
             
             plugin.setUpdateAsked(false);
             
